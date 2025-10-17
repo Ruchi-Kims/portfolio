35d403fa804eee96430f5713fe0f0d3894ce4497
Ruchi KIMPOLO <ruchi@i-techia.com>
2025-10-17 10:02:43 +0100

    Modification section hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,11 +7,11 @@ function Hero() {
     <div className={styles.content}>
         <h1 className={styles.title}>Hello, je me présente</h1>
         <p className={styles.description}>Développeur passionné créatif,je développe des applications web pour rendre vos idées réelles.
-        Mon objectif est de concevoir des interfaces modernes avec une expérience utilisateur agréable qui marie design élégant et fonctionnalité robuste. <br /><br /> Explorez mon univers numérique...</p>
+        Mon objectif est de concevoir des interfaces modernes avec une expérience utilisateur agréable qui marie design élégant et fonctionnalité robuste & fluide. <br /><br /> Explorez mon univers numérique...</p>
         <a href="mailto:ruchikimpolo@gmail.com" className={styles.contactBtns}>Contactez-moi</a>
     </div>
     
-   <img /*src='/assets/hero/imageProfil.jpeg'*/ alt='' className={styles.heroImage} />
+   <img src='/assets/hero/imageProfil.jpeg' alt='' className={styles.heroImage} />
     <div className={styles.topBlur} />
     <div className={styles.bottomBlur} />
 
